a15c51e2539b02e30ee1afad7960b4594cb4c31a
SerjNickey <serjnickey@gmail.com>
2020-01-08 02:05:57 +0300

    first commit

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,7 +4,7 @@ import AddBill from '../AddBill/AddBill'
 import { BillContext } from '../../Context/BillContext'
 import BillList from '../BillList/BillList'
 import BillTotal from '../BillTotal/BillTotal'
-import BillOptions from '../BillOptions./BillOptions'
+import BillOptions from '../BillOptions/BillOptions'
 import EditBills from '../EditBills/EditBills'
 
 
